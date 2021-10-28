dc08bd946d2ac5556411cccdd0e14af5862244c9
shreyasramakrishna90 <shreyasramakrishna90@gmail.com>
2021-10-28 17:47:19 -0500

    folders

--- carla-challange/dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:3
-WORKDIR /usr/src/app
-COPY . .
-CMD ["test.py"]
-ENTRYPOINT ["python3"]
---
+++ carla-challange/sdl/scene/Random.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import csv
+import pandas as pd
+import random
+import numpy as np
+
+
+
+def Random_Search(current_hyperparameters,folder,simulation_run,root,y):
+    """
+    The random sampler takes in the hyperparameters of the current step and returns a new hyperparameter set that is randomly sampled
+    """
+    new_hyperparameters_list = []
+    choices_array = []
+    distributions = []
+    if simulation_run <= 0:
+        for i in range(len(current_hyperparameters)):
+            if current_hyperparameters[i][0] == 'sun_altitude_angle':
+                parameter_distribution = 45.0
+            elif current_hyperparameters[i][0] == 'precipitation':
+                parameter_distribution = 0.0
+            elif current_hyperparameters[i][0] == 'cloud':
+                parameter_distribution = 0.0
+            else:
+                parameter_distribution = 0.0
+            distributions.append(int(parameter_distribution))
+            new_hyperparameters_list.append((current_hyperparameters[i][0],int(parameter_distribution)))
+    else:
+        for i in range(len(current_hyperparameters)):
+            if current_hyperparameters[i][0] == 'segment_number' or 'traffic' or 'camera_faults':
+                step = 1
+            else:
+                step = 5
+            #if current_hyperparameters[i][1] == current_hyperparameters[i][2]:
+            #    parameter_distribution = current_hyperparameters[i][1]
+            #else:
+            choice_list = np.arange(current_hyperparameters[i][1],current_hyperparameters[i][2],step)
+            choices_array.append(choice_list)
+            parameter_distribution = random.choice(choice_list)
+            distributions.append(int(parameter_distribution))
+            new_hyperparameters_list.append((current_hyperparameters[i][0],int(parameter_distribution)))
+
+
+    return distributions, new_hyperparameters_list
