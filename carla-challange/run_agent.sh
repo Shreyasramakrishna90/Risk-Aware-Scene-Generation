64b0bca07a306530f6578b33326535f7a3d6c339
shreyasramakrishna90 <shreyasramakrishna90@gmail.com>
2021-11-19 10:22:31 -0600

    update run_agent script

--- carla-challange/run_agent.sh
+++ carla-challange/run_agent.sh
@@ -4,10 +4,12 @@ trap "kill 0" EXIT
 k=1
 l=1
 
+#CARLA_PATH="/isis/Carla/CARLA_0.9.9"
+
 if [[ -n "$1" ]]; then
     end=$1
 else
-    end=2
+    end=4
 fi
 
 if [[ -n "$2" ]]; then
@@ -16,17 +18,24 @@ else
     exploration_runs=0
 fi
 
-textx generate sdl/scene/carla.tx --target dot
-textx generate sdl/scene/scene-model.carla --grammar sdl/scene/carla.tx --target dot
-textx generate sdl/scene/agent-model.carla --grammar sdl/scene/carla.tx --target dot
-
-export CARLA_ROOT=/home/scope/Carla/CARLA_0.9.9
+export CARLA_ROOT=/isis/Carla/CARLA_0.9.9
 export PORT=3000
 export ROUTES=leaderboard/data/routes/route_19.xml
 export TEAM_AGENT=image_agent.py
 export TEAM_CONFIG=carla_project/model.ckpt
 export HAS_DISPLAY=1
 total_scenes=$end
+
+#Initialize carla
+$CARLA_ROOT/CarlaUE4.sh -quality-level=Epic -world-port=3000 -resx=800 -resy=600 -opengl &
+PID=$!
+echo "Carla PID=$PID"
+sleep 10
+
+textx generate sdl/scene/carla.tx --target dot
+textx generate sdl/scene/scene-model.carla --grammar sdl/scene/carla.tx --target dot
+textx generate sdl/scene/agent-model.carla --grammar sdl/scene/carla.tx --target dot
+
 
 for (( j=0; j<=$end-1; j++ ))
   do
@@ -69,5 +78,8 @@ for (( j=0; j<=$end-1; j++ ))
     --project_path='/isis/Carla/iccps/'
 
 done
-python3 leaderboard/team_code/plot-stats.py \
---cutoff=${cutoff} \
+# python3 leaderboard/team_code/plot-stats.py \
+# --cutoff=${cutoff} \
+echo "Done CARLA Simulations"
+
+pkill -f "CarlaUE4"
